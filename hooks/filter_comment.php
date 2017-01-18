05c9f5ab974fe809b372defe9c303adf580e7cf1
fromlaptop <475685396@qq.com>
2017-01-18 14:59:07 +0800

    update hooks

--- hooks/filter_comment.php
+++ hooks/filter_comment.php
@@ -7,17 +7,15 @@ $pwd = `pwd`;
 //echo $pwd;
 
 // 获取当前分支名 是否是master
-
-
 $current_branch=`git branch  -vv | sed '/\*/!d'| awk -F" "  '{print $2}'`;
-var_dump(trim($current_branch));
+//var_dump(trim($current_branch));
 
 
 // 获取触发合并的命令值
 $mc=`git reflog | head -1 | awk  -F":" '{print $2}'`;
 $merge_command=trim($mc," :");
 
-var_dump($merge_command);
+//var_dump($merge_command);
 
 $command=explode(" ",$merge_command);
 // 检查是否是： git merge dev master
